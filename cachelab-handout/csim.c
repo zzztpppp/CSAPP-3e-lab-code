c26aa90525137c329a0d12319ea093b90bdf552c
azu <cxztpcom@gmail.com>
2020-08-31 18:04:02 +0800

    Add function to parse string representation of heximal address values.

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,17 +1,101 @@
 #include "cachelab.h"
 #include <getopt.h>
 #include <stdlib.h>
+#include <string.h>
 
-FILE *infile
+/* Helper function to calculated unsigned 
+ * integer power
+ */
+unsigned long ulong_power(unsigned long x, unsigned long y) {
+    unsigned long i = 0u;
+    unsigned long result = 1;
+    for (;i < y; i++){
+        result = result * x;
+    }
+
+    return result;
+}
+
+/* Helper function to convert 4-bit hex to unsigned int. */
+unsigned long hex_to_ulong(char c) {
+    unsigned long result;
+
+    if ((c >= 48) && (c <= 57)){
+        result = c - 48;
+    }
+
+    else{
+        if ((c >= 65) && (c <= 70)){
+            result = c-55;
+        }
+        else {
+            printf("Arguement %c is not a valid hex value\n", c);
+            exit(1);
+        }
+    }
+
+    return result;
+}
+
+/* Given a string representation of 8byte heximal 
+ * to unsigned long integer */
+ unsigned long convert_hex_string(char * hex_string) {
+     int length = strlen(hex_string);
+     unsigned long result = 0u;
+
+     for(unsigned long i = 0u; i < length; i++){
+        result = ulong_power(16u, i) * hex_to_ulong(hex_string[i]) + result;
+     }
+
+     return result;
+ }
+
+
+ /* Mask and extract bit values as unsigned int on a 64-bit address.
+  * given the bit posistion. Which is used for extracting set address
+  * and tag values of a 64-bit address */
+ unsigned long extract_bit_as_uint(unsigned long address, int low, int high){
+     
+     int width, bits_to_left, bits_to_right;
+     unsigned long mask = 0xFFFFFFFF;
+
+     if (low > high){
+         printf("Arugment low must not be greater than arguement high\n");
+         exit(1);
+     }
+     if ((low < 0) || (high) > 64){
+         printf("Arguement low and high must be in between 0 and 64");
+     }
+
+     width = high - low + 1;
+     bits_to_left = low;
+     bits_to_right = 64 - high;
+
+     /* Craft the mask for the corresponding address segment */
+     mask = ((mask << bits_to_left) >> (bits_to_left + bits_to_right)) << bits_to_right;  
+
+     return (mask & address) >> bits_to_right; 
+ }
+
+
+
+/* Given the memory address, 
+ * return the corresponding index of the cache */
+int * get_cache_location(char* addr, int num_set_bits, int associativity) {
+    unsigned long addr_unint = covert_hex_string(addr);
+}
+
 
 int main(int argc, char *argv[])
 {
     int num_set_bits = -1;
     int associativity = -1;
-    int num_block_bits = -1
+    int num_block_bits = -1;
     int verbose_flag = 0;
     int help_flag = 0;
     char *input_file_name;
+
+    int * cache_sim;
 
     int c;
     
@@ -31,7 +115,7 @@ int main(int argc, char *argv[])
                 associativity = atoi(optarg);
                 break;
             case 'b':
-                num_block_bits = atoi(optarg)
+                num_block_bits = atoi(optarg);
                 break;
             case 't':
                 input_file_name = optarg;
@@ -56,7 +140,12 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    /* The simulated cache */
+    cache_sim = 
+
     printSummary(0, 0, 0);
     return 0;
 }
+
+
 
