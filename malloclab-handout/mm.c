3e202fa7268ecd031a79f0318b780d9efbaf9e7d
azu <cxztpcom@gmail.com>
2020-12-27 23:19:07 +0800

    Add code in mm_init

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -14,6 +14,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 #include "mm.h"
 #include "memlib.h"
@@ -77,18 +78,115 @@ team_t team = {
 
 static const int num_free_list = 25;
 
-void *free_lists_array[num_free_list];
+
+// Each free list contains  2^i < size  <= 2^(i+1) of free blocks
+static void *free_lists_array[num_free_list];
+
+static void *heap_listp;
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    // Initialize empty free array
+    // Initialize the empty free list array
     for (int i=0; i < num_free_list; i++){
         free_lists_array[i] = (void *)-1;
     }
+
+    // Create the initial empty heap
+    if ((heap_listp = mem_sbrk(6*WSIZE)) == (void *)-1)
+        return -1;
+    
+    PUT(heap_listp, 0); // Alignment padding
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); // Prologue header
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); // Prologue footer
+    PUT(heap_listp + (3*WSIZE), PACK(DSIZE, 1)); // Epilogue header
+    
+    heap_listp = heap_listp + (2*WSIZE);
+    
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
     return 0;
+}
+
+/* extend_heap - Extend heap list by words.
+ */
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    /* Allocate an even number of words to maintain alignment */
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    /* Initialize free block header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
+    PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+
+    /* Coalesce if the previous block was free */
+    return coalesce(bp);
+}
+
+/* coalesce - Coalesce two adjacent emply blocks.
+ *  After coalescing, put the resulting block at proper 
+ *  free list class.
+ */
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc) { /* Case 1 */
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc) { /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size,0));
+    }
+
+    else if (!prev_alloc && next_alloc) { /* Case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    else { /* Case 4 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+        GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+        return bp;
+}
+
+
+/* put_block - Put the free block bp at the right place of free_list_array
+ *   according to the block size.
+ */
+static void put_block(void *bp){
+    void *free_listp;
+    size_t size  = GET_SIZE(HDRP(bp)) / DSIZE;
+    int size_class;
+    
+    // Find the proper size class
+    for(int i=0; i < num_free_list; i++){
+         if (size <= (1 << i)){
+             size_class = i;
+             break;
+         }
+    }
+
+    // Insert the block into free list of the size class
+
 }
 
 /* 
