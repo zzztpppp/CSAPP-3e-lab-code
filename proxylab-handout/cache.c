22b547a8afb3941495f5e6d9979de76528194dd2
azu <cxztpcom@gmail.com>
2021-05-03 22:35:03 +0800

    Implement a concurrent cache for proxy

--- proxylab-handout/cache.c
+++ proxylab-handout/cache.c
@@ -5,7 +5,7 @@
 
 int cache_init(cache_t *cache, size_t n) {
     int i;
-    cache->cache = Calloc(sizeof(kv_t), n);
+    cache->cache = Calloc(sizeof(kv_t *), n);
     cache->size = n;
     cache->time = Calloc(sizeof(int), n);
     Sem_init(&cache->mutex, 0, 1);
@@ -21,6 +21,56 @@ int cache_deinit(cache_t *cache) {
     return 0;
 }
 
+
+/* Lcok the caceh and perform insertion
+ */ 
 int insert(cache_t *cache, char *key, char *val, size_t size) {
-    
+    P(&cache->w);
+    kv_t *obj;
+    int to_insert = 0;
+    int least_time = cache->time[0];
+
+    // First find for an empty slot, if no found, evict 
+    // the slot that is least recently used.
+    for (int i = 1; i < cache->size; i++) {
+        if (cache->time[i] < least_time) {
+            to_insert = i;
+            least_time = cache->time[i];
+
+            /* Evict the memory use of old object*/
+            Free(cache->cache[to_insert]);
+        }
+    }
+
+    cache->time[to_insert]++;
+    obj = Malloc(sizeof(kv_t));
+    strncpy(obj->key, key, 50);
+    memcpy(obj->val, val, size);
+    cache->cache[to_insert] = obj;
+    V(&cache->w);
+    return 0;
+}
+
+
+/*
+ * find - Find the value of given key
+ *     in the given cache.
+ */
+kv_t *find(cache_t *cache, char *key) {
+    P(&cache->readcnt);
+    cache->readcnt++;
+    if (cache->readcnt == 1) 
+        P(&cache->w);
+    V(&cache->mutex);
+
+    /* Linearly search the entire cache */
+    int i;
+    kv_t *obj;
+    for (i = 0; i < cache->size; i++) {
+        obj = cache->cache[i];
+        if (!strcmp(key, obj->key)) {
+            return obj;
+        }
+    }
+    return NULL;
 }
