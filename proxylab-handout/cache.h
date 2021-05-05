aed2b592f46050f0c05c10ab2451786d3ae2a7e7
azu <cxztpcom@gmail.com>
2021-05-05 14:04:54 +0800

    Add functions hearders.

--- proxylab-handout/cache.h
+++ proxylab-handout/cache.h
@@ -3,17 +3,23 @@
  *************************************/
 
 typedef struct {
-    char *key;
-    char *val;
+    char key[50];
+    char val[102400];
+    size_t size;
 } kv_t;
 
 
 typedef struct  
 {
-    kv_t *cache;
+    kv_t **cache;
     int *time;
     int size;
     sem_t mutex, w;
     int readcnt;
 } cache_t;
 
+
+int insert(cache_t *cache, char *key, char *val, size_t size);
+int find(cache_t *cache, char *key, kv_t *kv);
+int cache_deinit(cache_t *cache);
+int cache_init(cache_t *cache, size_t n);
